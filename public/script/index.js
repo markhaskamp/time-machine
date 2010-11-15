cd3ba5fc047e815634602cb18eca6b9ebc636adb
Mark Haskamp <mrhask@gmail.com>
2010-11-14 22:29:25 -0500

    refactoring

--- public/script/index.js
+++ public/script/index.js
@@ -1,7 +1,9 @@
-var ea = new EventAggregator();
-var page_loader = new Page_Loader();
+var ea;
+var page_loader;
 
 $(document).ready( function() {
+        ea = new EventAggregator();
+        var page_loader = new Page_Loader();
         page_loader.page_load(ea);
     });
 
@@ -10,7 +12,7 @@ var Index_View = {
 
     , foo: function() {
         this.ndx++;
-        $('.main_timer').html(this.ndx);
+        $('#start_time_display').html(this.ndx);
     }
 
 }
