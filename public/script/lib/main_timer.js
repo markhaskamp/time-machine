0bbed035d78969d349d28644ac73675ea3c1b904
Mark Haskamp <mrhask@gmail.com>
2010-11-15 22:27:29 -0500

    display date string in #start_time_display

--- public/script/lib/main_timer.js
+++ public/script/lib/main_timer.js
@@ -5,16 +5,44 @@ var Main_Timer = {
     ,state: 'dormant'
 
     ,receive_tick: function() {
-        Main_Timer_View.update_start_time();
-        
+        var d = new Date();
+        Main_Timer_View.update_start_time_millis(d.getTime());
+        Main_Timer_View.update_start_time_display(format_date_for_display(d));
     }
-
-
 
 };
 
 var Main_Timer_View = {
-    update_start_time: function() {
+    update_start_time_millis: function(millis) {
+        $('#start_time_millis').html(millis);
+    }
+    , update_start_time_display: function(date_string) {
+        $('#start_time_display').html(date_string);
+    }
+};
+
+function format_date_for_display(d) {
+    var minutes_string = format_minutes_string(d.getMinutes());
+
+    var date_string = ((d.getMonth() + 1)) + '/' +
+                      d.getDate() + '/' +
+                      d.getFullYear() + ' - ' +
+        d.getHours() +
+        minutes_string
+        + ':' + d.getSeconds()
+        ;
+
+    return date_string;
+}
+
+function format_minutes_string(total_minutes) {
+    var minutes_string = ":";
+
+    if (total_minutes < 10) {
+        minutes_string += '0' + total_minutes;
+    } else {
+        minutes_string += total_minutes; 
     }
 
-};
+    return minutes_string;
+}
--- tests/jasmine/spec/Main_Timer.spec.js
+++ tests/jasmine/spec/Main_Timer.spec.js
@@ -3,13 +3,21 @@ describe("Main_Timer", function() {
                 expect(Main_Timer.state).toEqual(Main_Timer.state_dormant);
             });
 
-        it("given dormant, when receiving a tick event, updates the start_time", function() {
-                spyOn(Main_Timer_View, 'update_start_time');
+        it("given dormant, when receiving a tick event, updates #start_time_millis", function() {
+                spyOn(Main_Timer_View, 'update_start_time_millis');
 
                 Main_Timer.state = Main_Timer.state_dormant;
                 Main_Timer.receive_tick();
                 
-                expect(Main_Timer_View.update_start_time).toHaveBeenCalled();
+                expect(Main_Timer_View.update_start_time_millis).toHaveBeenCalled();
             });
 
+        it("given dormant, when receiving a tick event, updates #start_time_display", function() {
+                spyOn(Main_Timer_View, 'update_start_time_display');
+
+                Main_Timer.state = Main_Timer.state_dormant;
+                Main_Timer.receive_tick();
+                
+                expect(Main_Timer_View.update_start_time_display).toHaveBeenCalled();
+            });
     });
