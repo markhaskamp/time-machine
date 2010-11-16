d4e23655d2ce3c5d919cbbef7ceaebd173d5c324
Mark Haskamp <mrhask@gmail.com>
2010-11-15 22:42:21 -0500

    added images

--- tests/jasmine/spec/Main_Timer.spec.js
+++ tests/jasmine/spec/Main_Timer.spec.js
@@ -3,7 +3,7 @@ describe("Main_Timer", function() {
                 expect(Main_Timer.state).toEqual(Main_Timer.state_dormant);
             });
 
-        it("given dormant, when receiving a tick event, updates #start_time_millis", function() {
+        it("given state = dormant, when receiving a tick event, updates #start_time_millis", function() {
                 spyOn(Main_Timer_View, 'update_start_time_millis');
 
                 Main_Timer.state = Main_Timer.state_dormant;
@@ -12,12 +12,23 @@ describe("Main_Timer", function() {
                 expect(Main_Timer_View.update_start_time_millis).toHaveBeenCalled();
             });
 
-        it("given dormant, when receiving a tick event, updates #start_time_display", function() {
+        it("given state = dormant, when receiving a tick event, updates #start_time_display", function() {
                 spyOn(Main_Timer_View, 'update_start_time_display');
 
                 Main_Timer.state = Main_Timer.state_dormant;
                 Main_Timer.receive_tick();
                 
                 expect(Main_Timer_View.update_start_time_display).toHaveBeenCalled();
+            });
+
+        // when dormant, stop button is not displayed
+        // on start click, call hide_start_button
+
+        it("given state = running, when receiving a tick event, update #stop_time_millies", function() {
+                expect(false).toEqual(true);
+            });
+
+        it("given state = running, when receiving a tick event, update #stop_time_display", function() {
+                expect(false).toEqual(true);
             });
     });
